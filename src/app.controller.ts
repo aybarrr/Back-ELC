e9ffb80ec6b948a7baed823041d03334c7b43c39
Aybar Shora <aybarshora@gmail.com>
2023-10-30 04:05:11 +0600

    ai controller json

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -22,8 +22,9 @@ export class AiController {
   constructor(private readonly appService: AppService, private readonly googleAIService: GoogleAIService) {}
 
   @Post('define-tense')
-async generateText(@Body('input') input: string): Promise<string> {
-  return this.googleAIService.generateText(input);
+async generateText(@Body('input') input: string): Promise<object> {
+  const data = { tense: this.googleAIService.generateText(input)};
+  return data;
 }
 
  
