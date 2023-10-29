2e1cd07102b1479c7118a933af8c2accee77a591
Aybar Shora <aybarshora@gmail.com>
2023-10-30 02:17:24 +0600

    cors fix

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'http://127.0.0.1:5500/main.html'
+    ],
+    methods: ["GET", "POST"],
+    credentials: true,
+  });
   await app.listen(3000);
 }
 bootstrap();
